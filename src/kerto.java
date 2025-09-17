198a1e18e5cf42c3becebaf4ffba5a221b5a7fc8
joonastaimisto-dotcom <joonas.taimisto@gmail.com>
2025-09-17 16:42:06 +0300

    0.1
    Lisätty tulostin ohjelma.

---
+++ src/kerto.java
@@ -0,0 +1,16 @@
+public class kerto {
+    public static void main(String[] args) throws Exception {
+        String tekija = "Joonas";
+        int luku1 =5 ;
+        int luku2 =10 ;
+        int tulo = luku1 * luku2;
+        int luku3 = 500;
+        if (luku3>500) 
+  System.out.println("Mene baariin");
+
+
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.println("Ohjelman " + tekija);
+        System.out.println("" " = " tulo);
+    }
+}
---
+++ src/summa.java
@@ -0,0 +1,12 @@
+public class summa {
+    public static void main(String[] args) throws Exception {
+        String tekija = "Joonas";
+        int luku1 =5 ;
+        int luku2 =2 ;
+        //int tulo = luku1 + luku2;
+       
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.println("Ohjelman " + tekija);
+        System.out.println(luku1 + luku2);
+    }
+}
